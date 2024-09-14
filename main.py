c4df0e243182560b447afc64e81ad44c4360048f
Peter Huynh <peterhyh@yahoo.com>
2024-09-13 23:14:56 -0500

    chatbot function added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
-
+from difflib import get_close_matches
 
 # loading knowledge_base.json to this file (program)
+
+
 def load_knowledge_base(file_path: str) -> dict:
     with open(file_path, "r") as file:
         data: dict = json.load(file)
@@ -17,4 +19,46 @@ def save_knowledge_base(file_path: str, data: dict):
 # finding the best match from knowledge_base.json
 
 
-def find_best_match(user_question: str, questions: list[str]) -> str:
+def find_best_match(user_question: str, questions: list[str]) -> str | None:
+    matches: list = get_close_matches(
+        user_question, questions, n=1, cutoff=0.7)
+    return matches[0] if matches else None
+
+
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["question"]:
+        if q["question"] == question:
+            return q["answer"]
+
+
+# programming the chatbot functionality
+def chat_bot():
+    knowledge_base: dict = load_knowledge_base("knowledge_base.json")
+
+    while True:
+        user_input: str = input("You: ")
+
+        if user_input.lower() == "quit":
+            break
+
+        # Searching for the best match in knowledge_base.json
+        best_match: str | None = find_best_match(
+            user_input, [q["question"] for q in knowledge_base["questions"]])
+
+        # Bot searching for the answer and responding to user
+        if best_match:
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+            print(f"Bot: {answer}")
+        else:
+            print("Bot: I don't know this answer. Can you teach me?")
+            new_answer: str = input('Type the answer or "no" to skip: ')
+
+            if new_answer.lower() is not 'skip':
+                knowledge_base["questions"].append(
+                    {"question": user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print("Thank you! I have learned a new thing!")
+
+
+if __name__ == '__main__':
+    chat_bot()
