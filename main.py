37e9c42b4be329717f8f35fe57bc4ce7986f0f87
Peter Huynh <peterhyh@yahoo.com>
2024-09-14 00:22:09 -0500

    chat indentation added

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import json
 from difflib import get_close_matches
 
+
 # loading knowledge_base.json to this file (program)
-
-
 def load_knowledge_base(file_path: str):
     with open(file_path, "r") as file:
         data: dict = json.load(file)
     return data
 
+
 # saving responses as dictionary to knowledge_base.json
-
-
 def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
+
 # finding the best match from knowledge_base.json
-
-
 def find_best_match(user_question: str, questions: list[str]):
     matches: list = get_close_matches(
         user_question, questions, n=1, cutoff=0.7)
@@ -47,16 +44,16 @@ def chat_bot():
         # Bot searching for the answer and responding to user
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
-            print(f"Bot: {answer}")
+            print(f"\nBot: {answer}\n")
         else:
-            print("Bot: My apologies, I didn't understand. Could you teach me?")
+            print("\nBot: My apologies, I didn't understand. Could you teach me?\n")
             new_answer: str = input('Type the answer or "no" to skip: ')
 
             if new_answer.lower() != 'no':
                 knowledge_base["questions"].append(
                     {"question": user_input, "answer": new_answer})
                 save_knowledge_base('knowledge_base.json', knowledge_base)
-                print("Thank you! I have learned a new thing!")
+                print("\nBot: Thank you! I have learned a new thing!\n")
 
 
 if __name__ == '__main__':
